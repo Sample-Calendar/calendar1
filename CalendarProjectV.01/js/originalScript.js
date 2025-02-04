4c6cd7538f0a94c78af67bb2ba6abbb96057822c
Sample-Calendar <sayath.ji+calendar@gmail.com>
2025-02-03 18:55:36 -0600

    undoChanges

--- CalendarProjectV.01/js/originalScript.js
+++ CalendarProjectV.01/js/originalScript.js
@@ -1,11 +1,7 @@
 // js/originalscript.js
-//  import { events } from './eventData.js';
 
 document.addEventListener('DOMContentLoaded', function() {
-    const calendarEl = document.getElementById('calendar');
-
-  
-    
+    const calendarEl = document.getElementById('calendar')
     const calendar = new FullCalendar.Calendar(calendarEl, {
       initialView: 'dayGridMonth',initialDate: '2025-01-07',
       headerToolbar: {
