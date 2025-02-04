c20bc3767872fdc162af8e96573ac46e6f2b5c80
Sample-Calendar <sayath.ji+calendar@gmail.com>
2025-02-03 18:50:30 -0600

    movingEventsToseperatefile

---
+++ CalendarProjectV.01/js/eventData.js
@@ -0,0 +1,14 @@
+// js/eventData.js
+
+export const events = [
+    {
+      title: 'All Day Event',
+      start: '2025-01-01'
+    },
+    {
+      title: 'Long Event',
+      start: '2025-01-07',
+      end: '2025-01-10'
+    },
+    // ... other events
+  ];
