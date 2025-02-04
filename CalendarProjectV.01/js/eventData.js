7e98e21a7c5c87903514e1827ab1a86ee51ce598
Sample-Calendar <sayath.ji+calendar@gmail.com>
2025-02-03 18:58:07 -0600

    addModule

--- CalendarProjectV.01/js/eventData.js
+++ CalendarProjectV.01/js/eventData.js
@@ -10,5 +10,41 @@ export const events = [
       start: '2025-01-07',
       end: '2025-01-10'
     },
-    // ... other events
+    {
+      groupId: '999',
+      title: 'Repeating Event',
+      start: '2025-01-09T16:00:00'
+    },
+    {
+      groupId: '999',
+      title: 'Repeating Event',
+      start: '2025-01-16T16:00:00'
+    },
+    {
+      title: 'Conference',
+      start: '2025-01-11',
+      end: '2025-01-13'
+    },
+    {
+      title: 'Meeting',
+      start: '2025-01-12T10:30:00',
+      end: '2025-01-12T12:30:00'
+    },
+    {
+      title: 'Lunch',
+      start: '2025-01-12T12:00:00'
+    },
+    {
+      title: 'Meeting',
+      start: '2025-01-12T14:30:00'
+    },
+    {
+      title: 'Birthday Party',
+      start: '2025-01-13T07:00:00'
+    },
+    {
+      title: 'Click for Google',
+      url: 'https://google.com/',
+      start: '2025-01-28'
+    }
   ];
