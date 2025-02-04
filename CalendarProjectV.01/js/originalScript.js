b74c73050724f5c0c1f5f62460cdf8cc77491a76
Sample-Calendar <sayath.ji+calendar@gmail.com>
2025-02-03 18:54:17 -0600

    revertModules

--- CalendarProjectV.01/js/originalScript.js
+++ CalendarProjectV.01/js/originalScript.js
@@ -1,11 +1,11 @@
 // js/originalscript.js
-import { events } from './eventData.js';
+//  import { events } from './eventData.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     const calendarEl = document.getElementById('calendar');
 
-   
-
+  
+    
     const calendar = new FullCalendar.Calendar(calendarEl, {
       initialView: 'dayGridMonth',initialDate: '2025-01-07',
       headerToolbar: {
@@ -13,7 +13,54 @@ document.addEventListener('DOMContentLoaded', function() {
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
-      events: events
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2025-01-01'
+        },
+        {
+          title: 'Long Event',
+          start: '2025-01-07',
+          end: '2025-01-10'
+        },
+        {
+          groupId: '999',
+          title: 'Repeating Event',
+          start: '2025-01-09T16:00:00'
+        },
+        {
+          groupId: '999',
+          title: 'Repeating Event',
+          start: '2025-01-16T16:00:00'
+        },
+        {
+          title: 'Conference',
+          start: '2025-01-11',
+          end: '2025-01-13'
+        },
+        {
+          title: 'Meeting',
+          start: '2025-01-12T10:30:00',
+          end: '2025-01-12T12:30:00'
+        },
+        {
+          title: 'Lunch',
+          start: '2025-01-12T12:00:00'
+        },
+        {
+          title: 'Meeting',
+          start: '2025-01-12T14:30:00'
+        },
+        {
+          title: 'Birthday Party',
+          start: '2025-01-13T07:00:00'
+        },
+        {
+          title: 'Click for Google',
+          url: 'https://google.com/',
+          start: '2025-01-28'
+        }
+      ]
     })
     calendar.render()
-  });
+  })
